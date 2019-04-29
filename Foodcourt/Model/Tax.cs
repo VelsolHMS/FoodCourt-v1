5308131ea8d5e27dfa9c665aa0c3c2ad616157e3
GuduruUmesh <35020243+GuduruUmesh@users.noreply.github.com>
2019-04-29 15:51:20 +0530

    changes

--- Foodcourt/Model/Tax.cs
+++ Foodcourt/Model/Tax.cs
@@ -28,13 +28,31 @@ namespace Foodcourt.Model
             list.AddSqlParameter("@TAX_Percentage", TAX_Percentage);
             list.AddSqlParameter("@Tax_ReportingName", Tax_ReportingName);
             list.AddSqlParameter("@Tax_InsertBy", login.u);
-            list.AddSqlParameter("@Tax_InsertDate", DateTime.Today.ToShortDateString());
-            list.AddSqlParameter("@Tax_UpdateBy", login.u);
-            list.AddSqlParameter("@Tax_UpdateDate", DateTime.Today.ToShortDateString());
+            list.AddSqlParameter("@Tax_InsertDate", DateTime.Today.Date);
 
-            string s = "INSERT INTO FCTAX(TAX_Name,TAX_Percentage,Tax_ReportingName,Tax_InsertBy,Tax_InsertDate,Tax_UpdateBy,Tax_UpdateDate)" +
-                       "VALUES (@TAX_Name,@TAX_Percentage,@Tax_ReportingName,@Tax_InsertBy,@Tax_InsertDate,@Tax_UpdateBy,@Tax_UpdateDate)";
+            string s = "INSERT INTO FCTAX(TAX_Name,TAX_Percentage,Tax_ReportingName,Tax_InsertBy,Tax_InsertDate)" +
+                       "VALUES (@TAX_Name,@TAX_Percentage,@Tax_ReportingName,@Tax_InsertBy,@Tax_InsertDate)";
             DbFunctions.ExecuteCommand<DataTable>(s, list);
+        }
+        public DataTable GetTax()
+        {
+            var list = new List<SqlParameter>();
+            //select TAX_ID,TAX_Name,Tax_ReportingName,TAX_Percentage from FCTAX
+            string s = "Select TAX_ID,TAX_Name,Tax_ReportingName,TAX_Percentage from FCTAX";
+            DataTable dt = DbFunctions.ExecuteCommand<DataTable>(s, list);
+            return dt;
+        }
+        public void Update()
+        {
+            var lists = new List<SqlParameter>();
+            lists.AddSqlParameter("@TAX_Name", TAX_Name);
+            lists.AddSqlParameter("@TAX_ID", TAX_ID);
+            lists.AddSqlParameter("@Tax_ReportingName", Tax_ReportingName);
+            lists.AddSqlParameter("@TAX_Percentage", TAX_Percentage);
+            lists.AddSqlParameter("@Tax_UpdateBy", login.u);
+            lists.AddSqlParameter("@Tax_UpdateDate", DateTime.Today.Date);
+            string s = "Update FCTAX set TAX_Name = @TAX_Name, Tax_ReportingName = @Tax_ReportingName, TAX_Percentage = @TAX_Percentage, Tax_UpdateBy = @Tax_UpdateBy, Tax_UpdateDate = @Tax_UpdateDate where TAX_ID = @TAX_ID";
+            DbFunctions.ExecuteCommand<int>(s, lists);
         }
         public DataTable GetTaxNames()
         {
