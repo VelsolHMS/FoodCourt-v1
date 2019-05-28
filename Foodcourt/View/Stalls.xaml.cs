c6959c628cf54c9af6c07c13ee8c82ba5970783c
GuduruUmesh <35020243+GuduruUmesh@users.noreply.github.com>
2019-05-28 19:18:07 +0530

    changes

---
+++ Foodcourt/View/Stalls.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Foodcourt.View
+{
+    /// <summary>
+    /// Interaction logic for Stalls.xaml
+    /// </summary>
+    public partial class Stalls : Page
+    {
+        public int error = 0;
+        public Stalls()
+        {
+            InitializeComponent();
+        }
+        private void Validation_Error(object sender, ValidationErrorEventArgs e)
+        {
+            if (e.Action == ValidationErrorEventAction.Added)
+                error++;
+            else
+                error--;
+        }
+        private void BtnClear_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Dgstall_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
